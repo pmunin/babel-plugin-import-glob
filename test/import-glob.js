413a77a6f16446d1da20b13ac0292d1a031675e7
pmunin <philipp.munin@gmail.com>
2020-08-03 13:55:09 -0400

    fixing single2double quotes in tests + adding mystories test

--- test/import-glob.js
+++ test/import-glob.js
@@ -1,21 +1,22 @@
-import { resolve as resolvePath } from 'path'
-import test from 'ava'
-import { transform as babelTransform } from '@babel/core'
+import { resolve as resolvePath } from "path"
+import test from "ava"
+import { transform as babelTransform } from "@babel/core"
 
-function transform (code) {
+
+function transform(code) {
   return babelTransform(code, {
     babelrc: false,
     filename: __filename,
     sourceRoot: __dirname,
-    plugins: [resolvePath(__dirname, '..')]
+    plugins: [resolvePath(__dirname, "..")]
   }).code
 }
 
-function attempt (code) {
+function attempt(code) {
   return new Promise(resolve => resolve(transform(code)))
 }
 
-function check (msg) {
+function check(msg) {
   const preface = `${__filename}: `
   return err => {
     return err instanceof SyntaxError &&
@@ -24,131 +25,131 @@ function check (msg) {
   }
 }
 
-test('throws if import does not contain a pattern', async t => {
+test("throws if import does not contain a pattern", async t => {
   await t.throws(
-    attempt("import { foo } from 'glob:'"),
-    check("Missing glob pattern 'glob:'"))
+    attempt(`import { foo } from "glob:"`),
+    check(`Missing glob pattern "glob:"`))
 })
 
-test('throws if pattern is absolute', async t => {
+test("throws if pattern is absolute", async t => {
   await t.throws(
-    attempt("import { foo } from 'glob:/folder/*'"),
-    check("Glob pattern must be relative, was '/folder/*'"))
+    attempt(`import { foo } from "glob:/folder/*"`),
+    check(`Glob pattern must be relative, was "/folder/*"`))
 })
 
-test('throws if pattern is not relative', async t => {
+test("throws if pattern is not relative", async t => {
   await t.throws(
-    attempt("import { foo } from 'glob:folder/*'"),
-    check("Glob pattern must be relative, was 'folder/*'"))
+    attempt(`import { foo } from "glob:folder/*"`),
+    check(`Glob pattern must be relative, was "folder/*"`))
 })
 
-test('throws if no glob: prefix and does not start with .', async t => {
+test("throws if no glob: prefix and does not start with .", async t => {
   await t.throws(
-    attempt("import { foo } from 'glob:folder/*'"),
-    check("Glob pattern must be relative, was 'folder/*'"))
+    attempt(`import { foo } from "glob:folder/*"`),
+    check(`Glob pattern must be relative, was "folder/*"`))
 })
 
-test('throws if a member identifier cannot be generated', async t => {
+test("throws if a member identifier cannot be generated", async t => {
   await t.throws(
-    attempt("import * as members from 'glob:./fixtures/cannot-generate-identifier/*.txt'"),
-    check("Could not generate a valid identifier for './fixtures/cannot-generate-identifier/-.txt'"))
+    attempt(`import * as members from "glob:./fixtures/cannot-generate-identifier/*.txt"`),
+    check(`Could not generate a valid identifier for "./fixtures/cannot-generate-identifier/-.txt"`))
 })
 
-test('throws if members collide', async t => {
+test("throws if members collide", async t => {
   await t.throws(
-    attempt("import { fooBar } from 'glob:./fixtures/member-collision/*.txt'"),
-    check("Found colliding members 'fooBar'"))
+    attempt(`import { fooBar } from "glob:./fixtures/member-collision/*.txt"`),
+    check(`Found colliding members "fooBar"`))
 })
 
-test('throws if imports cannot be mapped', async t => {
+test("throws if imports cannot be mapped", async t => {
   await t.throws(
-    attempt("import { baz } from 'glob:./fixtures/foo-bar/*.txt'"),
-    check("Could not match import 'baz' to a module. Available members are 'fooBar'"))
+    attempt(`import { baz } from "glob:./fixtures/foo-bar/*.txt"`),
+    check(`Could not match import "baz" to a module. Available members are "fooBar"`))
 })
 
-test("cannot map 'toString'", async t => {
+test(`cannot map "toString"`, async t => {
   await t.throws(
-    attempt("import { toString } from 'glob:./fixtures/foo-bar/*.txt'"),
+    attempt(`import { toString } from "glob:./fixtures/foo-bar/*.txt"`),
     SyntaxError)
 })
 
-test('throws when importing the default member', async t => {
+test("throws when importing the default member", async t => {
   await t.throws(
-    attempt("import fooBar from 'glob:./fixtures/foo-bar/*.txt'"),
-    check('Cannot import the default member'))
+    attempt(`import fooBar from "glob:./fixtures/foo-bar/*.txt"`),
+    check("Cannot import the default member"))
 })
 
-test('rewrites the import statement', t => {
+test("rewrites the import statement", t => {
   t.is(
-    transform("import { foo, bar } from 'glob:./fixtures/multiple/*.txt'"),
-`import foo from './fixtures/multiple/foo.txt';
-import bar from './fixtures/multiple/bar.txt';`)
+    transform(`import { foo, bar } from "glob:./fixtures/multiple/*.txt"`),
+    `import foo from "./fixtures/multiple/foo.txt";
+import bar from "./fixtures/multiple/bar.txt";`)
 })
 
-test('does not require glob prefix', t => {
+test("does not require glob prefix", t => {
   t.is(
-    transform("import { foo, bar } from './fixtures/multiple/*.txt'"),
-    `import foo from './fixtures/multiple/foo.txt';
-import bar from './fixtures/multiple/bar.txt';`)
+    transform(`import { foo, bar } from "./fixtures/multiple/*.txt"`),
+    `import foo from "./fixtures/multiple/foo.txt";
+import bar from "./fixtures/multiple/bar.txt";`)
 })
 
-test('does not transform standard import', t => {
+test("does not transform standard import", t => {
   t.is(
-    transform("import foo from 'fixtures/multiple/foo.txt'"),
-    `import foo from 'fixtures/multiple/foo.txt';`)
+    transform(`import foo from "fixtures/multiple/foo.txt"`),
+    `import foo from "fixtures/multiple/foo.txt";`)
 })
 
-test('constructs the member by identifierfying the captured file name parts', t => {
+test("constructs the member by identifierfying the captured file name parts", t => {
   t.is(
-    transform("import { fooBar, bazQux } from 'glob:./fixtures/extnames/*.txt'"),
-    `import fooBar from './fixtures/extnames/foo.bar.txt';
-import bazQux from './fixtures/extnames/baz.qux.txt';`)
+    transform(`import { fooBar, bazQux } from "glob:./fixtures/extnames/*.txt"`),
+    `import fooBar from "./fixtures/extnames/foo.bar.txt";
+import bazQux from "./fixtures/extnames/baz.qux.txt";`)
 })
 
-test('constructs the member by identifierfying captured directory components, separating them by dollar signs', t => {
+test("constructs the member by identifierfying captured directory components, separating them by dollar signs", t => {
   t.is(
-    transform("import { fooBar$baz, qux$quux } from 'glob:./fixtures/subdirectories/**/*.txt'"),
-    `import fooBar$baz from './fixtures/subdirectories/foo-bar/baz.txt';
-import qux$quux from './fixtures/subdirectories/qux/quux.txt';`)
+    transform(`import { fooBar$baz, qux$quux } from "glob:./fixtures/subdirectories/**/*.txt"`),
+    `import fooBar$baz from "./fixtures/subdirectories/foo-bar/baz.txt";
+import qux$quux from "./fixtures/subdirectories/qux/quux.txt";`)
 })
 
-test('constructs the member by identifierfying captured directory components, without unnecessary underscores', t => {
+test("constructs the member by identifierfying captured directory components, without unnecessary underscores", t => {
   t.is(
     // eslint-disable-next-line max-len
-    transform("import { noUnnecessaryUnderscores$new, noUnnecessaryUnderscores$42 } from 'glob:./fixtures/subdirectories/**/*.txt'"),
-    `import noUnnecessaryUnderscores$new from './fixtures/subdirectories/no-unnecessary-underscores/new.txt';
-import noUnnecessaryUnderscores$42 from './fixtures/subdirectories/no-unnecessary-underscores/42.txt';`)
+    transform(`import { noUnnecessaryUnderscores$new, noUnnecessaryUnderscores$42 } from "glob:./fixtures/subdirectories/**/*.txt"`),
+    `import noUnnecessaryUnderscores$new from "./fixtures/subdirectories/no-unnecessary-underscores/new.txt";
+import noUnnecessaryUnderscores$42 from "./fixtures/subdirectories/no-unnecessary-underscores/42.txt";`)
 
   t.is(
-    transform("import { _new, _42 } from 'glob:./fixtures/necessary-underscores/*.txt'"),
-    `import _new from './fixtures/necessary-underscores/new.txt';
-import _42 from './fixtures/necessary-underscores/42.txt';`)
+    transform(`import { _new, _42 } from "glob:./fixtures/necessary-underscores/*.txt"`),
+    `import _new from "./fixtures/necessary-underscores/new.txt";
+import _42 from "./fixtures/necessary-underscores/42.txt";`)
 })
 
-test('normalizes the source path irrespective of pattern', t => {
+test("normalizes the source path irrespective of pattern", t => {
   t.is(
-    transform("import { fooBar } from 'glob:../test/fixtures/foo-bar/*.txt'"),
-    "import fooBar from './fixtures/foo-bar/foo-bar.txt';")
+    transform(`import { fooBar } from "glob:../test/fixtures/foo-bar/*.txt"`),
+    `import fooBar from "./fixtures/foo-bar/foo-bar.txt";`)
 })
 
-test('supports importing directories', t => {
+test("supports importing directories", t => {
   t.is(
-    transform("import { extnames, fooBar } from 'glob:./fixtures/*'"),
-    `import extnames from './fixtures/extnames';
-import fooBar from './fixtures/foo-bar';`)
+    transform(`import { extnames, fooBar } from "glob:./fixtures/*"`),
+    `import extnames from "./fixtures/extnames";
+import fooBar from "./fixtures/foo-bar";`)
 })
 
-test('supports aliasing members', t => {
+test("supports aliasing members", t => {
   t.is(
-    transform("import { fooBar as fb } from 'glob:./fixtures/foo-bar/*.txt'"),
-    "import fb from './fixtures/foo-bar/foo-bar.txt';")
+    transform(`import { fooBar as fb } from "glob:./fixtures/foo-bar/*.txt"`),
+    `import fb from "./fixtures/foo-bar/foo-bar.txt";`)
 })
 
-test('supports importing the entire glob pattern as a namespace object', t => {
+test("supports importing the entire glob pattern as a namespace object", t => {
   t.is(
-    transform("import * as members from 'glob:./fixtures/multiple/*.txt'"),
-    `import _members_bar from './fixtures/multiple/bar.txt';
-import _members_foo from './fixtures/multiple/foo.txt';
+    transform(`import * as members from "glob:./fixtures/multiple/*.txt"`),
+    `import _members_bar from "./fixtures/multiple/bar.txt";
+import _members_foo from "./fixtures/multiple/foo.txt";
 const members = {
   bar: _members_bar,
   foo: _members_foo
@@ -156,18 +157,18 @@ const members = {
 Object.freeze(members);`)
 })
 
-test('supports importing modules for their side-effects', t => {
+test("supports importing modules for their side-effects", t => {
   t.is(
-    transform("import 'glob:./fixtures/multiple/*.txt'"),
-    `import './fixtures/multiple/bar.txt';
-import './fixtures/multiple/foo.txt';`)
+    transform(`import "glob:./fixtures/multiple/*.txt"`),
+    `import "./fixtures/multiple/bar.txt";
+import "./fixtures/multiple/foo.txt";`)
 })
 
-test('sub-extension removed because it is in the pattern', t => {
+test("sub-extension removed because it is in the pattern", t => {
   t.is(
-    transform("import * as members from 'glob:./fixtures/ext-from-pattern/*.foo.txt'"),
-    `import _members_one from './fixtures/ext-from-pattern/one.foo.txt';
-import _members_two from './fixtures/ext-from-pattern/two.foo.txt';
+    transform(`import * as members from "glob:./fixtures/ext-from-pattern/*.foo.txt"`),
+    `import _members_one from "./fixtures/ext-from-pattern/one.foo.txt";
+import _members_two from "./fixtures/ext-from-pattern/two.foo.txt";
 const members = {
   one: _members_one,
   two: _members_two
@@ -175,11 +176,11 @@ const members = {
 Object.freeze(members);`)
 })
 
-test('sub-extension not removed because it is not in the pattern', t => {
+test("sub-extension not removed because it is not in the pattern", t => {
   t.is(
-    transform("import * as members from 'glob:./fixtures/ext-from-pattern/*.txt'"),
-    `import _members_oneFoo from './fixtures/ext-from-pattern/one.foo.txt';
-import _members_twoFoo from './fixtures/ext-from-pattern/two.foo.txt';
+    transform(`import * as members from "glob:./fixtures/ext-from-pattern/*.txt"`),
+    `import _members_oneFoo from "./fixtures/ext-from-pattern/one.foo.txt";
+import _members_twoFoo from "./fixtures/ext-from-pattern/two.foo.txt";
 const members = {
   oneFoo: _members_oneFoo,
   twoFoo: _members_twoFoo
@@ -187,13 +188,13 @@ const members = {
 Object.freeze(members);`)
 })
 
-test('test braced paths', t => {
+test("test braced paths", t => {
   t.is(
-    transform("import * as members from 'glob:./fixtures/use-dir-name/{!(sub),@(sub)/*}/foo.txt'"),
-    `import _members_one from './fixtures/use-dir-name/one/foo.txt';
-import _members_sub$four from './fixtures/use-dir-name/sub/four/foo.txt';
-import _members_sub$three from './fixtures/use-dir-name/sub/three/foo.txt';
-import _members_two from './fixtures/use-dir-name/two/foo.txt';
+    transform(`import * as members from "glob:./fixtures/use-dir-name/{!(sub),@(sub)/*}/foo.txt"`),
+    `import _members_one from "./fixtures/use-dir-name/one/foo.txt";
+import _members_sub$four from "./fixtures/use-dir-name/sub/four/foo.txt";
+import _members_sub$three from "./fixtures/use-dir-name/sub/three/foo.txt";
+import _members_two from "./fixtures/use-dir-name/two/foo.txt";
 const members = {
   one: _members_one,
   sub$four: _members_sub$four,
@@ -203,12 +204,12 @@ const members = {
 Object.freeze(members);`)
 })
 
-test('extension should still be ommitted within brace', t => {
+test("extension should still be ommitted within brace", t => {
   t.is(
-    transform("import * as members from 'glob:./fixtures/ext-from-pattern/*.{txt,csv}'"),
-    `import _members_oneFoo from './fixtures/ext-from-pattern/one.foo.txt';
-import _members_three from './fixtures/ext-from-pattern/three.csv';
-import _members_twoFoo from './fixtures/ext-from-pattern/two.foo.txt';
+    transform(`import * as members from "glob:./fixtures/ext-from-pattern/*.{txt,csv}"`),
+    `import _members_oneFoo from "./fixtures/ext-from-pattern/one.foo.txt";
+import _members_three from "./fixtures/ext-from-pattern/three.csv";
+import _members_twoFoo from "./fixtures/ext-from-pattern/two.foo.txt";
 const members = {
   oneFoo: _members_oneFoo,
   three: _members_three,
@@ -217,10 +218,10 @@ const members = {
 Object.freeze(members);`)
 })
 
-test('includes directory names between * parts in the member names', t => {
+test("includes directory names between * parts in the member names", t => {
   t.is(
-    transform("import * as members from './fixtures/*/qux/*.txt'"),
-    `import _members_subdirectories$qux$quux from './fixtures/subdirectories/qux/quux.txt';
+    transform(`import * as members from "./fixtures/*/qux/*.txt"`),
+    `import _members_subdirectories$qux$quux from "./fixtures/subdirectories/qux/quux.txt";
 const members = {
   subdirectories$qux$quux: _members_subdirectories$qux$quux
 };
@@ -228,11 +229,11 @@ Object.freeze(members);`
   )
 })
 
-test('excludes non-captured brace expansion parts in the member names', t => {
+test("excludes non-captured brace expansion parts in the member names", t => {
   t.is(
-    transform("import * as members from './fixtures/subdirectories/{foo-bar,qux}/*.txt'"),
-    `import _members_baz from './fixtures/subdirectories/foo-bar/baz.txt';
-import _members_quux from './fixtures/subdirectories/qux/quux.txt';
+    transform(`import * as members from "./fixtures/subdirectories/{foo-bar,qux}/*.txt"`),
+    `import _members_baz from "./fixtures/subdirectories/foo-bar/baz.txt";
+import _members_quux from "./fixtures/subdirectories/qux/quux.txt";
 const members = {
   baz: _members_baz,
   quux: _members_quux
@@ -240,3 +241,30 @@ const members = {
 Object.freeze(members);`
   )
 })
+
+
+// test("my stories", t => {
+
+//   const path = require('path')
+//   const glob = require('glob')
+//   const capture = require('minimatch-capture')
+//   const identifierfy = require('identifierfy')
+
+//   const pattern = "../**/*.stories?(.js|)";
+
+//   // console.warn({__dirname:__dirname});
+//   // console.warn(glob.sync(pattern, {cwd:__dirname, }));
+
+//   console.warn(
+//   transform(`import * as stories from "glob:${pattern}"`),
+//   );
+
+//    //t.is(
+//      /** 
+// [ '../test/fixtures/myStories/component1/component1.stories.js',
+//   '../test/fixtures/myStories/component2/component2.stories',
+//   '../test/fixtures/myStories/my.stories.js' ]
+//      */
+// //     `import fooBar$baz from "./fixtures/subdirectories/foo-bar/baz.txt";
+//  //import qux$quux from "./fixtures/subdirectories/qux/quux.txt";`);
+// });
